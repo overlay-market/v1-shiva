a20e670333b777e65d4b236d2ac1d20c20442b65
Ezequiel <eperez@itba.edu.ar>
2024-05-03 04:11:43 -0300

    feat: add Shiva interface

---
+++ src/IShiva.sol
@@ -0,0 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+import {IOverlayV1Market} from "./IOverlayV1Market.sol";
+
+interface IShiva {
+    error NotPositionOwner();
+
+    function ownerOf(
+        IOverlayV1Market market,
+        uint256 positionId
+    ) external view returns (address);
+
+    function build(
+        IOverlayV1Market market,
+        uint256 collateral,
+        uint256 leverage,
+        bool isLong,
+        uint256 priceLimit
+    ) external returns (uint256 positionId);
+
+    function buildOnBehalfOf(
+        IOverlayV1Market market,
+        address owner,
+        bytes calldata signature,
+        uint256 collateral,
+        uint256 leverage,
+        bool isLong,
+        uint256 priceLimit
+    ) external returns (uint256 positionId);
+
+    function unwind(
+        IOverlayV1Market market,
+        uint256 positionId,
+        uint256 fraction,
+        uint256 priceLimit
+    ) external;
+
+    function unwindOnBehalfOf(
+        IOverlayV1Market market,
+        address owner,
+        bytes calldata signature,
+        uint256 positionId,
+        uint256 fraction,
+        uint256 priceLimit
+    ) external;
+}
