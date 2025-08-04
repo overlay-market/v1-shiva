88df8f6ea161176aa8aa0d028ead8287f5857857
djaciel <dj_roses@hotmail.com>
2025-08-04 11:36:05 -0600

    refactor: implement REWARD_VAULT_BALANCE_VALIDATION flag for conditional balance assertions in tests

--- test/ShivaLocal.t.sol
+++ test/ShivaLocal.t.sol
@@ -223,7 +223,9 @@ contract ShivaLocalTest is Test, ShivaTestBase, ShivaTest {
         vm.startPrank(bob);
         vm.expectRevert();
         shiva.overlayMarketLiquidateCallback(posId);
-        assertNotEq(rewardVault.balanceOf(alice), 0);
+        if (REWARD_VAULT_BALANCE_VALIDATION) {
+            assertNotEq(rewardVault.balanceOf(alice), 0);
+        }
     }
 
     /**
@@ -241,6 +243,6 @@ contract ShivaLocalTest is Test, ShivaTestBase, ShivaTest {
         impersonator.impersonateLiquidation(
             address(shiva), posId, uint96(leverage.mulDown(collateral))
         );
-        assertEq(rewardVault.balanceOf(alice), leverage.mulUp(collateral));
+        assertEq(rewardVault.balanceOf(alice), REWARD_VAULT_BALANCE_VALIDATION ? leverage.mulUp(collateral) : 0);
     }
 }
